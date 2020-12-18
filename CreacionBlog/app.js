c6933eb58ee1bd99b7a11eeb7a2ea7c398af9bdf
Alexis-Diaz <alex.d4556@gmail.com>
2020-12-18 14:16:52 -0600

    Se implemento manejo de rutas con File System (fs) y envio de html en la peticion GET

--- CreacionBlog/app.js
+++ CreacionBlog/app.js
@@ -1,26 +1,43 @@
 const http = require('http');//se importa el servicio http. Este es un paqute incluido en node js
-const { listenerCount } = require('process');
+const fs = require('fs')//modulo por defecto de node file system para trabajar con archivos
 
-// //FORMA 1
-// const hostname = 'localhost';
-// const port = 3000;
-
-// //Esta funcion sirve de servidor para la aplicacion. La callback tiene los parametros de request y respond (peticion y respuesta)
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;//Este es un estado que se envia. Existen diferentes numeros que representan estados diferentes. En caso de no asignarse por defecto toma 200 ok
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hola Mundo');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-// });
-
-//FORMA 2
 const server = http.createServer((req, res)=>{
-  res.writeHead(200,{"Content-Type":"application/json"})//Aqui se escribe una cabecera el 200 es el estado y content-type el tipo de respuesta enviada.
-  res.write('{mensaje: "Hola me llamaste"}')
+
+  if(req.method==='GET' && req.url==="/index.html"){
+    return index(req, res)
+  }
+
+  if(req.method==='GET' && req.url==="/nosotros.html"){
+    return nosotros(req, res)
+  }
+
+  res.write('Hola 1')
   res.end();
 })
 
+function index(req, res){
+  const stat = fs.statSync('./index.html');
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': stat.size//en vez de escribir un numero stat.size me da el tamaño automatico del archivo
+  })
+
+  const readStream = fs.createReadStream('./index.html')//tema a investigar que son los stream
+  //pipe es pegarlo a otro
+  readStream.pipe(res);
+  //no se necesita re.ende porque cuando createReadStream termine el proceso cerrara automaticamente la llamada.
+}
+
+function nosotros (req, res){
+  const stat = fs.statSync('./nosotros.html');
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': stat.size//en vez de escribir un numero stat.size me da el tamaño automatico del archivo
+  })
+
+  const readStream = fs.createReadStream('./nosotros.html')//tema a investigar que son los stream
+  //pipe es pegarlo a otro
+  readStream.pipe(res);
+  //no se necesita re.ende porque cuando createReadStream termine el proceso cerrara automaticamente la llamada.
+}
 server.listen(8080);
