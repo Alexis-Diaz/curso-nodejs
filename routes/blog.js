d2e07be8c26bc74f02740579a786ae6d4878c0d8
Alexis-Diaz <alex.d4556@gmail.com>
2020-12-22 11:30:51 -0600

    Se creó la vista detail y se implemento el uso de parametros en la url para obtener el id

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,14 @@
 import express from 'express'
-
 import controller from '../controllers/blog.js'
-
+import bodyParser from 'body-parser'
 
 const router = express.Router()
 
+router.use(bodyParser.urlencoded({extended:true}));
 router.get('/', controller.renderBlog);
+router.get('/new-post', controller.renderNewPost);//se verifica en ruta que tipo de peticion es
+router.post('/new-post', controller.newPost)
+router.get('/detail/:id', controller.detail)//despues de los dos puntos express entiende que es un parametro
+
 
 export default router
